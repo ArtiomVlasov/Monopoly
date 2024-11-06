71cfccf0fa9c7fe647151576cb9b985f9a7b5a13
a.vlasov3 <a.vlasov3@g.nsu.ru>
2024-11-06 22:14:05 +0700

    described player class

--- src/player.hpp
+++ src/player.hpp
@@ -1,6 +1,6 @@
 #include <string>
-#include <list>
-#include "property.hpp"
+#include <vector>
+#include "classes.hpp"
 
 class Player
 {
@@ -8,9 +8,28 @@ private:
     std::string name;
     int balance;
     int position;
-    std::list<Property> listOfProperty;
+    std::vector<Property> listOfProperty;
+    bool inJael;
+    bool bankrupt;
+
 public:
-    Player();
-    void makeMove(int prevPos);
+    Player(std::string name);
+    void makeMove(int steps);
+    void addProperty(Property *property);
+    void pay(int amount);
+    void receive(int amount);
+    bool canAfford(int amount) const;
+    void mortgageProperty(Property *property);
+    void unmortgagedProperty(Property *property);
+    int getBalance() const;
+    std::string getName() const;
+    int getPosition() const;
+    const std::vector<Property *> &getProperties() const;
+    bool isBankrupt() const;
+    bool isInJail() const;
+    void sendToJail();
+    void releaseFromJail();
+    bool canBuildOn(Property *property) const;
+    void buildStructure(Property *property);   
     ~Player();
 };
