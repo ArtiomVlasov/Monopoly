ac7572a88fcfcf8ba0c9de0491954bb2559574ef
a.vlasov3 <a.vlasov3@g.nsu.ru>
2024-11-06 17:31:52 +0700

    init commit

---
+++ CMakeFiles/nsuoop-cpp-template-lab2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nsuoop-cpp-template-lab2.dir/lab2b.cpp.o"
+  "CMakeFiles/nsuoop-cpp-template-lab2.dir/lab2b.cpp.o.d"
+  "nsuoop-cpp-template-lab2"
+  "nsuoop-cpp-template-lab2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nsuoop-cpp-template-lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
