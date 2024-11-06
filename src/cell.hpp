3f57354159de51c3c79ac5c04a4089e5272808e3
a.vlasov3 <a.vlasov3@g.nsu.ru>
2024-11-06 22:16:11 +0700

    update cell metods

--- src/cell.hpp
+++ src/cell.hpp
@@ -1,6 +1,9 @@
+#include "player.hpp"
+
 class Cell
 {
  public:
-    virtual void onLand() = 0;
+    virtual void onLand() = 0;// сообщает на какую клетку попал
+    virtual void applyEffect(Player& player) = 0;
     virtual ~Cell() = default;  
 };
