bac98e8299ab8d307256321a461c2e7da37fc643
a.vlasov3 <a.vlasov3@g.nsu.ru>
2024-11-07 14:26:32 +0700

    add field CellType and method getType

--- src/cell.hpp
+++ src/cell.hpp
@@ -1,9 +1,15 @@
 #include "player.hpp"
 
+enum class CellType { Property, Chance, Tax, Prison, Street, Railway, Utilities};
+
 class Cell
 {
- public:
-    virtual void onLand() = 0;// сообщает на какую клетку попал
-    virtual void applyEffect(Player& player) = 0;
-    virtual ~Cell() = default;  
+protected:
+    CellType type;
+public:
+    Cell(CellType t);
+    virtual void onLand() = 0; // сообщает на какую клетку попал
+    CellType getType() const;
+    virtual void defaultAction(Player &player) = 0;
+    virtual ~Cell() = default;
 };
