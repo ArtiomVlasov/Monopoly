5b4830eb94a5cfc1b79c387c4092620534163d4d
localhost808O <91614028+localhost808O@users.noreply.github.com>
2024-11-08 22:22:57 +0700

    Update property.cpp

--- src/property.cpp
+++ src/property.cpp
@@ -5,6 +5,22 @@
 Property::Property(CellType type, std::string Name, int Price, int Rent, Player owner) 
     : name(Name), price(Price), rent(Rent), type(type), owner(owner) {}
 
+void Property::defaultAction(Player &player)  {
+    Cell* c = Board::getCell(player.getPosition());
+    if (player.isInJail() && player.getNumMovesInPrison() == 3) {
+        player.setNumMovesInPrison(0);
+        player.setInJael(false);  
+    } else if (!player.isBankrupt() && (Property::isOwned()) && (owner.getName() != player.getName())){ // если player попал на какую-то не свою клетку
+        payRent(player);
+    } else if (c->getType() == CellType::publicTreasury) {
+        //тут пикают карту и чет делают (это когда общ казна) 
+    }
+}
+
+void Property::startAction(Property& property) {
+    
+}
+
 void Property::buy(Player &player) {
     this->owner = player;
     player.setBalance(this->rent);
