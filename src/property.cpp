554c12c85738f3957c0d1378fce4b5e538193041
localhost808O <91614028+localhost808O@users.noreply.github.com>
2024-11-08 20:31:50 +0700

    Create property.cpp
    add file and add a base method
    (Not all methods are available yet)

---
+++ src/property.cpp
@@ -0,0 +1,39 @@
+#include "property.hpp"
+#include "cell.hpp"
+#include "board.hpp"
+
+Property::Property(CellType type, std::string Name, int Price, int Rent, Player owner) 
+    : name(Name), price(Price), rent(Rent), type(type), owner(owner) {}
+
+void Property::buy(Player &player) {
+    this->owner = player;
+    player.setBalance(this->rent);
+}
+
+void Property::payRent(Player &player) {
+    player.setBalance(getAmountOfRent());
+}
+
+int Property::getRent() {
+    return this->rent;
+}
+
+bool Property::isOwned() {
+    return !this->name.empty();
+}
+
+int Property::getPrice() const {
+    return this->price;
+}
+
+void Property::mortgage() {
+    this->owner.setBalance(this->owner.getBalance() * 1.1);
+    this->owner = Player();
+}
+
+void Property::unMortgage(Player &player) {
+    if(!player.isInJail() && !player.isBankrupt() && player.getBalance() == price*1.1) {
+        player.setBalance(player.getBalance() - this->price * 1.1);
+        this->owner = player;
+    }
+}
