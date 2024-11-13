4e4efcfa7868bf39269f409b4ceb2bcf3ea316be
a.vlasov3 <a.vlasov3@g.nsu.ru>
2024-11-14 01:46:12 +0700

    update defaultAction, mortgage, unMorgage

--- src/property.cpp
+++ src/property.cpp
@@ -1,22 +1,44 @@
 #include "property.hpp"
 #include "player.hpp"
 
-
 Property::Property(CellType type, std::string Name, int Price, int Rent, Player *owner)
     : Cell(type), name(Name), price(Price), rent(Rent), owner(owner), isMortgage(false) {}
 
-void Property::defaultAction(Player *player, Game* game)
+void Property::defaultAction(Player *player, Game *game)
 {
-    if (player->getName() != owner->getName() && (isMortgage == false))
+    if (player->getName() != owner->getName() && !isMortgage)
     {
-        payRent(player);
+        int rentAmount = calculateRent(player);
+
+        switch (player->canAfford(rentAmount))
+        {
+        case Player::AffordStatus::CAN_AFFORD:
+            std::cout << "Игрок " << player->getName() << " попал на имущество, принадлежащее " << owner->getName()
+                      << ". Заплатите ренту в размере " << rentAmount << " монет.\n";
+            payRent(player); // Выполняем оплату ренты
+            break;
+
+        case Player::AffordStatus::NEED_TO_SELL_PROPERTY:
+            std::cout << "Игрок " << player->getName() << " попал на имущество, принадлежащее " << owner->getName()
+                      << ", но недостаточно средств на балансе. Продайте часть имущества для оплаты ренты в размере "
+                      << rentAmount << " монет.\n";
+                    // какой то метод из Game наверно надо взять, он будет давать игроку возмежность сделать выбор что продать
+            break;
+
+        case Player::AffordStatus::CANNOT_AFFORD:
+            std::cout << "Игрок " << player->getName() << " не может позволить себе оплатить ренту. Объявляется банкротство.\n";
+            player->declareBankruptcy(owner);
+            return;
+        }
     }
-    else if(player->getName() != owner->getName() && (isMortgage == true)){
-        std::cout << player->getName();
-    }
-    else if (!player->canAfford(price))
-    
+    else if (player->getName() != owner->getName() && isMortgage)
     {
+        std::cout << "Игрок " << player->getName() << " попал на заложенное имущество. Рента не требуется.\n";
+        return;
+    }
+    else if (player->canAfford(price) == Player::AffordStatus::CANNOT_AFFORD)
+    {
+        std::cout << "У игрока " << player->getName() << " недостаточно средств для покупки. Имущество отправляется на аукцион.\n";
         player->startAuction(this, game->getListOfPlayers());
         return;
     }
@@ -24,12 +46,22 @@ void Property::defaultAction(Player *player, Game* game)
     switch (ans)
     {
     case 0: // может купить
-        player->buy(*this);
-        // player.buy()
-    case 1: // акуцион
+        std::cout << "Игрок " << player->getName() << " решил купить имущество за " << price << " монет.\n";
+        player->buy(this);
+        break;
+    case 1: // аукцион
+        std::cout << "Игрок " << player->getName() << " выставляет имущество на аукцион.\n";
         player->startAuction(this, game->getListOfPlayers());
-        return;
+        break;
     }
+}
+
+void Property::payRent(Player *player)
+{
+    int amount = calculateRent(player);
+    std::cout << "Игрок " << player->getName() << " платит ренту в размере " << amount << " монет игроку " << owner->getName() << ".\n";
+    player->pay(amount);
+    owner->receive(amount);
 }
 
 void Property::setOwner(Player *newOwner)
@@ -42,23 +74,10 @@ bool Property::isMortgaged()
     return isMortgage;
 }
 
-void Property::markAsAvailable(){
-    isMortgage = false;
-    //owner = &Player();
-}
-
-void Property::payRent(Player *player)
+void Property::markAsAvailable()
 {
-    int amount = calculateRent(player);
-    if (player->canAfford(amount))
-    {
-        player->pay(amount);
-        owner->receive(amount);
-    }
-    else
-    {
-        player->declareBankruptcy(owner);
-    }
+    isMortgage = false;
+    owner = nullptr;
 }
 
 int Property::getRent() const
@@ -76,45 +95,44 @@ int Property::getPrice() const
     return this->price;
 }
 
-std::string Property::getName() const{
+std::string Property::getName() const
+{
     return name;
 }
 
 void Property::mortgage()
 {
-    this->owner->setBalance(this->owner->getBalance() * 1.1);
+    this->isMortgage = true;
+    this->owner->receive(calculateMortgage());
 }
 
 void Property::unMortgage(Player *player)
 {
-    if (!player->isInJail() && !player->isBankrupt() && player->getBalance() == price * 1.1)
+    if (!player->isInJail() && !player->isBankrupt() && player->getBalance() >= calculateUnMortgage())
     {
-        player->setBalance(player->getBalance() - this->price * 1.1);
-        this->owner = player;
+        player->pay(calculateUnMortgage());
+        this->isMortgage = false;
     }
 }
 
-/*bool Property::isFullListOfProperty(Player &player, CellType type, CellType proptype)
+bool Property::isFullListOfProperty(Player *player, CellType type)
 {
-    int countQuantityPropertys = 0;
-    for (int index = 0; index < player.getQuantityOfProperty(); index++)
-    {
-        if (player.getCellTypeInListOfProperty(index) == type)
-        {
-            countQuantityPropertys++;
+    int countQuantityProperty = 0;
+    for (const Property* property : player->getProperties()) {
+        if (property->getType() == type) {
+            countQuantityProperty++;
         }
     }
-    return countQuantityPropertys == handleCellType(proptype);
-}*/
-
-int Property::getAmountOfRent() const
-{
-    return this->rent;
+    return countQuantityProperty == handleCellType(type);
 }
 
-int Property::getAmountOfMortgage() const
+int Property::calculateMortgage()
 {
-    return 0; // ????
+    return rent / 2;
+}
+int Property::calculateUnMortgage()
+{
+    return (rent / 2 + rent * 0.1);
 }
 
 Property::~Property()
