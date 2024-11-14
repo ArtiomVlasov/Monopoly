e058c4a3dd13ca0f91783dce81a6a26354b093b2
localhost808O <91614028+localhost808O@users.noreply.github.com>
2024-11-14 17:59:53 +0700

    Update board.hpp

--- src/board.hpp
+++ src/board.hpp
@@ -8,9 +8,6 @@ private:
     std::vector<std::shared_ptr<Cell>> cells;
 public:
     Board(int numCells);
-    Cell* getCell(int position) const;
+    std::shared_ptr<Cell> getCell(int position) const;
     int getNextPosition(int currentPosition, int roll) const;
-    void addCell(std::shared_ptr<Cell> cell);
-    const std::vector<std::unique_ptr<Cell>>& getAllCells() const;
-
 };
