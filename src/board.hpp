f4e5b89816b8b2597f49cc29e1ed5a26e19668b6
localhost808O <91614028+localhost808O@users.noreply.github.com>
2024-11-15 22:03:13 +0700

    Update board.hpp

--- src/board.hpp
+++ src/board.hpp
@@ -1,12 +1,15 @@
 #include <vector>
 #include <memory>
+#pragma once
 
 class Cell;
 
 class Board {
 private:
     std::vector<std::shared_ptr<Cell>> cells;
+    
 public:
+    Board();
     Board(int numCells);
     std::shared_ptr<Cell> getCell(int position) const;
     int getNextPosition(int currentPosition, int roll) const;
