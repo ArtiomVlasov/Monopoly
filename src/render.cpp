b6ade20f72ec4ebc805db3ee3453cfed8edc6b3a
localhost808O <91614028+localhost808O@users.noreply.github.com>
2024-11-15 22:05:01 +0700

    Update render.cpp

--- src/render.cpp
+++ src/render.cpp
@@ -1,10 +1,50 @@
 #include <iostream>
 #include <vector>
-#include <render.hpp>
-class Street;
-class Property;
 
-void Render::drawBoard()
+#include "player.hpp"
+#include "property.hpp"
+
+
+std::vector<std::string> listOFstreets = {
+    "FP", // Free Parking
+    "KA", // Kentucky Avenue
+    "CH", // Chance
+    "IA", // Indiana Avenue
+    "IL", // Illinois Avenue
+    "BR", // B&O Railroad
+    "AA", // Atlantic Avenue
+    "VA", // Ventnor Avenue
+    "WW", // Water Works
+    "MG", // Marvin Gardens
+    "GJ", // Go to Jail
+    "NY", // New York Avenue
+    "PA", // Pacific Avenue
+    "TA", // Tennessee Avenue
+    "NC", // North Carolina Avenue
+    "CC", // Community Chest
+    "SJ", // St. James Place
+    "PA", // Pennsylvania Avenue
+    "PR", // Pennsylvania Railroad
+    "SL", // Short Line
+    "VA", // Virginia Avenue
+    "SA", // States Avenue
+    "PP", // Park Place
+    "EC", // Electric Company
+    "LT", // LUXURY TAX
+    "SC", // St. Charles Place
+    "J",  // Jail
+    "BW", // Boardwalk
+    "JV", // Just Visiting
+    "CA", // Connecticut Avenue
+    "VA", // Vermont Avenue
+    "OA", // Oriental Avenue
+    "RR", // Reading Railroad
+    "CT", // City Tax
+    "BA", // Baltic Avenue
+    "MA"  // Mediterranean Avenue
+};
+
+void drawBoard()
 {
     int index = 0;
     for (int i = 0; i < 3; i++)
@@ -18,7 +58,7 @@ void Render::drawBoard()
             }
             else
             {
-                std::cout << "  " << properties[index] << "   |";
+                std::cout << "  " << listOFstreets[index] << "   |";
                 index++;
             }
         }
@@ -37,9 +77,9 @@ void Render::drawBoard()
             }
             else
             {
-                std::cout << " " << properties[index] << "   |";
+                std::cout << " " << listOFstreets[index] << "   |";
                 printf("\t\t\t\t\t\t\t\t\t  ");
-                std::cout << " " << properties[index + 1] << "   |" << std::endl;
+                std::cout << " " << listOFstreets[index + 1] << "   |" << std::endl;
                 index++;
                 index++;
                 continue;
@@ -58,7 +98,7 @@ void Render::drawBoard()
             }
             else
             {
-                std::cout << "  " << properties[index] << "   |";
+                std::cout << "  " << listOFstreets[index] << "   |";
                 index++;
             }
         }
@@ -66,7 +106,7 @@ void Render::drawBoard()
     }
 }
 
-void Render::displayPlayerInfo(const Player &player) const
+void displayPlayerInfo(const Player &player)
 {
     std::cout << "Игрок: " << player.getName() << "\n";
     std::cout << "Баланс: " << player.getBalance() << "\n";
@@ -81,7 +121,7 @@ void Render::displayPlayerInfo(const Player &player) const
     std::cout << "-------------------------------------\n";
 }
 
-void Render::RenderCreateCell(std::string s, int flag)
+void RenderCreateCell(std::string s, int flag)
 {
 
     for (int index = 0; index < 10; index++)
@@ -107,4 +147,29 @@ void Render::RenderCreateCell(std::string s, int flag)
             printf("          ");
         }
     }
+}
+
+void displayMenu()
+{   
+    std::cout << "Возможные действия в игре 'Монополия':n";
+    std::cout << "1. Продать собственность";
+    std::cout << "2. Построить дом/отель";
+    std::cout << "3. Выйти из тюрьмы";
+    std::cout << "4. Остаться в тюрьме";
+    std::cout << "10. Объявить банкротствоn";
+    std::cout << "11. Сделать предложение другим игрокамn";
+    std::cout << "12. Применить карточку 'Выход из тюрьмы бесплатно'n";
+    std::cout << "13. Сделать ход назад (если предусмотрено правилами)n";
+    std::cout << "14. Завершить ходn";
+}
+
+void WelcomeThePlayers(std::vector<Player *> players)
+{
+    std::cout << "____________________________________________________________________________________________________________________" << std::endl;
+    std::cout << "Welcome To Monopoly!!!!!" << std::endl;
+    std::cout << "____________________________________________________________________________________________________________________" << std::endl;
+    std::cout << "WARNING!!!!Before we start,please make the console window bigger in order to see the whole stuff...Width:140 and Height:60 should be OK!" << std::endl;
+    std::cout << "If you have done the previous step,we are about to start" << std::endl;
+    std::cout << "____________________________________________________________________________________________________________________" << std::endl;
+    
 }
