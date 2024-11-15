b18290b12c043ad876bcfc2cadcef41c64a68ef8
localhost808O <91614028+localhost808O@users.noreply.github.com>
2024-11-15 22:03:03 +0700

    Update board.cpp

--- src/board.cpp
+++ src/board.cpp
@@ -1,18 +1,19 @@
 #include "board.hpp"
-
+#include "property.hpp"
 #include "cell.hpp"
 
 Board::Board(int numCells)
 {
     for (int index = 0; index < numCells; index++)
     {
+        //Property(CellType type,std::string Name, int Price, int Rent, Player *owner);
         if (index == 21 || (index > 22 && index < 25))
         {
-            cells.push_back(std::make_shared<Cell>(CellType::RedStreet, index));
+            cells.push_back(std::make_shared<Property>(CellType::RedStreet, "NULL", 100, 100, nullptr));
         }
         else if (index == 29 || index == 27 || index == 26)
         {
-            cells.push_back(std::make_shared<Cell>(CellType::YellowStreet, index));
+            cells.push_back(std::make_shared<Cell>(CellType::YellowStreet));
         }
         else if (index == 31 || index == 32 || index == 34)
         {
@@ -20,59 +21,59 @@ Board::Board(int numCells)
         }
         else if (index == 39 || index == 37)
         {
-            cells.push_back(std::make_shared<Cell>(CellType::BlueStreet, index));
+            cells.push_back(std::make_shared<Cell>(CellType::BlueStreet));
         }
         else if (index == 39 || index == 37)
         {
-            cells.push_back(std::make_shared<Cell>(CellType::BrownStreet, index));
+            cells.push_back(std::make_shared<Cell>(CellType::BrownStreet));
         }
         else if (index == 19 || index == 18 || index == 16)
         {
-            cells.push_back(std::make_shared<Cell>(CellType::OrangeStreet, index));
+            cells.push_back(std::make_shared<Cell>(CellType::OrangeStreet));
         }
         else if (index == 11 || index == 13 || index == 14)
         {
-            cells.push_back(std::make_shared<Cell>(CellType::PinkStreet, index));
+            cells.push_back(std::make_shared<Cell>(CellType::PinkStreet));
         }
         else if (index == 9 || index == 8 || index == 6)
         {
-            cells.push_back(std::make_shared<Cell>(CellType::WhiteStreet, index));
+            cells.push_back(std::make_shared<Cell>(CellType::WhiteStreet));
         }
         else if (index == 5 || index == 15 || index == 25 || index == 35)
         {
-            cells.push_back(std::make_shared<Cell>(CellType::PropRailway, index));
+            cells.push_back(std::make_shared<Cell>(CellType::PropRailway));
         }
         else if (index == 2 || index == 18)
         {
-            cells.push_back(std::make_shared<Cell>(CellType::PropUtilities, index));
+            cells.push_back(std::make_shared<Cell>(CellType::PropUtilities));
         }
         else if (index == 7 || index == 22 || index == 36)
         {
-            cells.push_back(std::make_shared<Cell>(CellType::Chance, index));
+            cells.push_back(std::make_shared<Cell>(CellType::Chance));
         }
         else if (index == 34)
         {
-            cells.push_back(std::make_shared<Cell>(CellType::Tax, index));
+            cells.push_back(std::make_shared<Cell>(CellType::Tax));
         }
         else if (index == 10)
         {
-            cells.push_back(std::make_shared<Cell>(CellType::Prison, index));
+            cells.push_back(std::make_shared<Cell>(CellType::Prison));
         }
         else
         {
-            cells.push_back(std::make_shared<Cell>(CellType::publicTreasury, index));
+            cells.push_back(std::make_shared<Cell>(CellType::publicTreasury));
         }
     }
+
 }
 
 
 std::shared_ptr<Cell> Board::getCell(int position) const {
-    for (int index = 0; index < cells.size(); index++) {
-        if (cells[index]->getPosition() == position) {
-            return cells[index];
-        }
-    }
+    return cells[position];
 }
 
+int Board:: getNextPosition(int currentPosition, int roll) const {
+    return (currentPosition + roll) % cells.size();
+}
 
 
