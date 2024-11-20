7219b885d0920076e47875afa086e9381b60887b
a.vlasov3 <a.vlasov3@g.nsu.ru>
2024-11-20 08:52:47 +0700

    update constructors

--- src/board.cpp
+++ src/board.cpp
@@ -6,52 +6,52 @@
 
 Board::Board(int numCells, Game *game):game(game)
 {
-    cells.push_back(std::make_shared<Street>(CellType::BrownStreet, "Old Kent Road", 60, 2));
+    cells.push_back(std::make_shared<Street>("Old Kent Road", 60, 2, Street::Color::BrownStreet));
     cells.push_back(std::make_shared<PublicTreasury>());
-    cells.push_back(std::make_shared<Street>(CellType::BrownStreet, "Whitechapel Road", 60, 4));
+    cells.push_back(std::make_shared<Street>("Whitechapel Road", 60, 4, Street::Color::BrownStreet));
     cells.push_back(std::make_shared<Tax>(200));
-    cells.push_back(std::make_shared<Railway>(CellType::PropRailway, "King's Cross Station", 200, 25));
+    cells.push_back(std::make_shared<Railway>("King's Cross Station", 200, 25));
 
-    cells.push_back(std::make_shared<Street>(CellType::WhiteStreet, "The Angel Islington", 100, 6));
+    cells.push_back(std::make_shared<Street>("The Angel Islington", 100, 6, Street::Color::WhiteStreet));
     cells.push_back(std::make_shared<Chance>(game));
-    cells.push_back(std::make_shared<Street>(CellType::WhiteStreet, "Euston Road", 100, 6));
-    cells.push_back(std::make_shared<Street>(CellType::WhiteStreet, "Pentonville Road", 120, 8));
+    cells.push_back(std::make_shared<Street>("Euston Road", 100, 6,  Street::Color::WhiteStreet));
+    cells.push_back(std::make_shared<Street>("Pentonville Road", 120, 8,  Street::Color::WhiteStreet));
     cells.push_back(std::make_shared<EmptyCell>());
 
-    cells.push_back(std::make_shared<Street>(CellType::PinkStreet, "Pall Mall", 140, 10));
-    cells.push_back(std::make_shared<Utilities>(CellType::PropUtilities, "Electric Company", 150, 1));
-    cells.push_back(std::make_shared<Street>(CellType::PinkStreet, "Whitehall", 140, 10));
-    cells.push_back(std::make_shared<Street>(CellType::PinkStreet, "Northumberland Avenue", 160, 12));
-    cells.push_back(std::make_shared<Railway>(CellType::PropRailway, "Marylebone Station", 200, 25));
+    cells.push_back(std::make_shared<Street>("Pall Mall", 140, 10, Street::Color::PinkStreet));
+    cells.push_back(std::make_shared<Utilities>( "Electric Company", 150, 1));
+    cells.push_back(std::make_shared<Street>("Whitehall", 140, 10, Street::Color::PinkStreet));
+    cells.push_back(std::make_shared<Street>("Northumberland Avenue", 160, 12, Street::Color::PinkStreet));
+    cells.push_back(std::make_shared<Railway>( "Marylebone Station", 200, 25));
 
-    cells.push_back(std::make_shared<Street>(CellType::OrangeStreet, "Bow Street", 180, 14));
+    cells.push_back(std::make_shared<Street>("Bow Street", 180, 14, Street::Color::OrangeStreet));
     cells.push_back(std::make_shared<PublicTreasury>());
-    cells.push_back(std::make_shared<Street>(CellType::OrangeStreet, "Marlborough Street", 180, 14));
-    cells.push_back(std::make_shared<Street>(CellType::OrangeStreet, "Vine Street", 200, 16));
+    cells.push_back(std::make_shared<Street>("Marlborough Street", 180, 14, Street::Color::OrangeStreet));
+    cells.push_back(std::make_shared<Street>("Vine Street", 200, 16, Street::Color::OrangeStreet));
     cells.push_back(std::make_shared<EmptyCell>());
 
-    cells.push_back(std::make_shared<Street>(CellType::RedStreet, "Strand", 220, 18));
+    cells.push_back(std::make_shared<Street>("Strand", 220, 18, Street::Color::RedStreet));
     cells.push_back(std::make_shared<Chance>(game));
-    cells.push_back(std::make_shared<Street>(CellType::RedStreet, "Fleet Street", 220, 18));
-    cells.push_back(std::make_shared<Street>(CellType::RedStreet, "Trafalgar Square", 240, 20));
-    cells.push_back(std::make_shared<Railway>(CellType::PropRailway, "Fenchurch St Station", 200, 25));
+    cells.push_back(std::make_shared<Street>("Fleet Street", 220, 18, Street::Color::RedStreet));
+    cells.push_back(std::make_shared<Street>("Trafalgar Square", 240, 20, Street::Color::RedStreet));
+    cells.push_back(std::make_shared<Railway>( "Fenchurch St Station", 200, 25));
 
-    cells.push_back(std::make_shared<Street>(CellType::YellowStreet, "Leicester Square", 260, 22));
-    cells.push_back(std::make_shared<Street>(CellType::YellowStreet, "Coventry Street", 260, 22));
-    cells.push_back(std::make_shared<Utilities>(CellType::PropUtilities, "Water Works", 150, 1));
-    cells.push_back(std::make_shared<Street>(CellType::YellowStreet, "Piccadilly", 280, 24));
+    cells.push_back(std::make_shared<Street>("Leicester Square", 260, 22, Street::Color::YellowStreet));
+    cells.push_back(std::make_shared<Street>("Coventry Street", 260, 22, Street::Color::YellowStreet));
+    cells.push_back(std::make_shared<Utilities>( "Water Works", 150, 1));
+    cells.push_back(std::make_shared<Street>("Piccadilly", 280, 24, Street::Color::YellowStreet));
     cells.push_back(std::make_shared<Prison>());
 
-    cells.push_back(std::make_shared<Street>(CellType::GreenStreet, "Regent Street", 300, 26));
-    cells.push_back(std::make_shared<Street>(CellType::GreenStreet, "Oxford Street", 300, 26));
+    cells.push_back(std::make_shared<Street>("Regent Street", 300, 26, Street::Color::GreenStreet));
+    cells.push_back(std::make_shared<Street>("Oxford Street", 300, 26, Street::Color::GreenStreet));
     cells.push_back(std::make_shared<PublicTreasury>());
-    cells.push_back(std::make_shared<Street>(CellType::GreenStreet, "Bond Street", 320, 28));
-    cells.push_back(std::make_shared<Railway>(CellType::PropRailway, "Liverpool Street Station", 200, 25));
+    cells.push_back(std::make_shared<Street>("Bond Street", 320, 28, Street::Color::GreenStreet));
+    cells.push_back(std::make_shared<Railway>( "Liverpool Street Station", 200, 25));
 
     cells.push_back(std::make_shared<Chance>(game));
-    cells.push_back(std::make_shared<Street>(CellType::BlueStreet, "Park Lane", 350, 35));
+    cells.push_back(std::make_shared<Street>("Park Lane", 350, 35, Street::Color::BlueStreet));
     cells.push_back(std::make_shared<Tax>(100));
-    cells.push_back(std::make_shared<Street>(CellType::BlueStreet, "Mayfair", 400, 50));
+    cells.push_back(std::make_shared<Street>("Mayfair", 400, 50, Street::Color::BlueStreet));
     cells.push_back(std::make_shared<EmptyCell>());
 
 }
