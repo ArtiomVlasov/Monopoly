f88cbd5cdc4a85936c24c4394e38b0f3bc01881d
localhost808O <91614028+localhost808O@users.noreply.github.com>
2024-11-21 14:35:14 +0700

    Update render.cpp

--- src/render.cpp
+++ src/render.cpp
@@ -1,174 +1,341 @@
-// #include <iostream>
-// #include <vector>
-// #include "player.hpp"
-// #include "property.hpp"
+#include <iostream>
+#include <vector>
+#include "player.hpp"
+#include "property.hpp"
+#include "propertySubClasses.hpp"
+
+std::vector<std::pair<std::string, std::string>> listOFstreets = {
+    {"GO", "GO"},
+    {"Mediterranean Avenue", "MA"},
+    {"Community Chest", "CC"},
+    {"Baltic Avenue", "BA"},
+    {"Income Tax", "IT"},
+    {"Reading Railroad", "RR"},
+    {"Oriental Avenue", "OA"},
+    {"Chance", "CH"},
+    {"Vermont Avenue", "VA"},
+    {"Connecticut Avenue", "CA"},
+    {"Jail (Just Visiting)", "JV"},
+    {"St. Charles Place", "SC"},
+    {"Electric Company", "EC"},
+    {"States Avenue", "SA"},
+    {"Virginia Avenue", "VA"},
+    {"Pennsylvania Railroad", "PR"},
+    {"St. James Place", "SJ"},
+    {"Community Chest", "CC"},
+    {"Tennessee Avenue", "TA"},
+    {"New York Avenue", "NY"},
+    {"Jail (Just Visiting)", "JV"},
+    {"Kentucky Avenue", "KA"},
+    {"Chance", "CH"},
+    {"Indiana Avenue", "IA"},
+    {"Illinois Avenue", "IA"},
+    {"B&O Railroad", "BR"},
+    {"Atlantic Avenue", "AA"},
+    {"Ventnor Avenue", "VA"},
+    {"Water Works", "WW"},
+    {"Marvin Gardens", "MG"},
+    {"Pacific Avenue", "PA"},
+    {"North Carolina Avenue", "NC"},
+    {"Community Chest", "CC"},
+    {"Pennsylvania Avenue", "PA"},
+    {"Short Line Railroad", "SL"},
+    {"Chance", "CH"},
+    {"Park Place", "PP"},
+    {"Luxury Tax", "LT"},
+    {"Boardwalk", "BW"},
+    {"Jail (Just Visiting)", "JV"}};
+
+// Удаляем дубликаты и сортируем по порядку, если необходимо.
+
+void drawBoard()
+{
+    int index = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (i == 0 || i == 2)
+            {
+                printf("_______ ");
+                continue;
+            }
+            else
+            {
+                std::cout << "  " << listOFstreets[index].second << "   |";
+                index++;
+            }
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (j == 0 || j == 2)
+            {
+                printf("______");
+                printf("\t\t\t\t\t\t\t\t\t  ");
+                printf("_______\n");
+                continue;
+            }
+            else
+            {
+                std::cout << " " << listOFstreets[index].second << "   |";
+                printf("\t\t\t\t\t\t\t\t\t  ");
+                std::cout << " " << listOFstreets[index].second << "   |" << std::endl;
+                index++;
+                index++;
+                continue;
+            }
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (i == 0 || i == 2)
+            {
+                printf("_______ ");
+                continue;
+            }
+            else
+            {
+                std::cout << "  " << listOFstreets[index].second << "   |";
+                index++;
+            }
+        }
+        printf("\n");
+    }
+}
+
+void displayPlayerInfo(Player *player)
+{
+    std::cout << "Игрок: " << player->getName() << "\n";
+    std::cout << "Баланс: " << player->getBalance() << "\n";
+    std::cout << "Позиция на поле: " << player->getPosition() << "\n";
+    std::cout << "В тюрьме: " << (Prison::isInJail(player) ? "Да" : "Нет") << "\n";
+    std::cout << "Банкрот: " << (Game::isBankruptPlayer(player) ? "Да" : "Нет") << "\n";
+    std::cout << "Недвижимость:\n";
+    for (const Property *property : player->getProperties())
+    {
+        std::cout << " - " << property->getName() << "\n";
+    }
+    std::cout << "-------------------------------------\n";
+}
+
+void RenderCreateCell(std::string s, int flag)
+{
+
+    for (int index = 0; index < 10; index++)
+    {
+        if (flag == 1)
+        {
+            printf("                                             \n");
+        }
+        if (flag == 2)
+        {
+            printf("\n");
+        }
+        if (index == 0 || index == 9)
+        {
+            printf("____");
+        }
+        else if (index == 5)
+        {
+            printf("| %s |", s);
+        }
+        else
+        {
+            printf("          ");
+        }
+    }
+}
+
+void displayMenu()
+{
+    std::cout << "Возможные действия в игре 'Монополия':\n";
+    std::cout << "1. Продать собственность\n";
+    std::cout << "2. Построить дом/отель\n";
+    std::cout << "3. Выйти из тюрьмы\n";
+    std::cout << "4. Остаться в тюрьме\n";
+    std::cout << "5. Объявить банкротствоn\n";
+    std::cout << "6. Сделать предложение другим игрокам\n";
+    std::cout << "7. Применить карточку 'Выход из тюрьмы бесплатно'\n";
+    std::cout << "8. Сделать ход назад (если предусмотрено правилами)\n";
+    std::cout << "9. Завершить ход\n";
+}
+
+void WelcomeThePlayers(std::vector<Player *> players)
+{
+    std::cout << "____________________________________________________________________________________________________________________" << std::endl;
+    std::cout << "Welcome To Monopoly!!!!!" << std::endl;
+    std::cout << "____________________________________________________________________________________________________________________" << std::endl;
+    std::cout << "WARNING!!!!Before we start,please make the console window bigger in order to see the whole stuff...Width:140 and Height:60 should be OK!" << std::endl;
+    std::cout << "If you have done the previous step,we are about to start" << std::endl;
+    std::cout << "____________________________________________________________________________________________________________________" << std::endl;
+}
+
+void renderPayPlayer(Player *player, int amount)
+{
+    std::cout << "С баланса игрока " << player->getName() << " списано " << amount << " монет.\n";
+}
+
+void renderReceivePlayer(Player *player, int amount)
+{
+    std::cout << "Игрок " << player->getName() << " получает " << amount << " монет.\n";
+}
+
+void renderPlayerDeclareBankruptcy(Player *creditor)
+{
+    std::cout << "Игрок " << creditor->getName() << " не может оплатить ренту и объявляет банкротство.\n";
+}
+
+void renderPlayerDeclareBankruptcyIfCreditor(Player *creditor)
+{
+    std::cout << "Его имущество переходит к " << creditor->getName() << ".\n";
+}
+
+void renderPlayerDeclareBankruptcyIfCreditorGetPos(Player *creditor, Property *property)
+{
+    std::cout << property->getName() << " переходит к " << creditor->getName() << ".\n";
+}
+
+void propertyToBank()
+{
+    std::cout << "Его имущество возвращается банку и снова доступно для покупки.\n";
+}
+
+void renderPlayerCanBuildOn(Player *player, int flag)
+{
+    if (flag == 0)
+    {
+        std::cout << "Невозможно построить, так как это не улица.\n";
+    }
+    else if (flag == 1)
+    {
+        std::cout << "Улица не принадлежит игроку " << player->getName() << ".\n";
+    }
+    else if (flag == 2)
+    {
+        std::cout << "Игрок должен владеть всей цветовой группой, чтобы строить здесь.\n";
+    }
+    else if (flag == 3)
+    {
+        std::cout << "Невозможно построить, пока улица заложена.\n";
+    }
+}
+
+void renderPlayerBuildStructure(Street *street, Player *player)
+{
+    std::cout << "Игрок " << player->getName() << " построил здание на \"" << street->getName()
+              << "\". Уровень улицы теперь: " << street->getLevelOfStreet() << ".\n";
+}
+
+void renderPlayerNotBuildStructure(Street *street)
+{
+    std::cout << "Недостаточно средств для строительства на \"" << street->getName() << "\".\n";
+}
+
+void renderPlayerDestroyStructure(Street *street, Player *player, int status)
+{
+    if (status == 0)
+    {
+        std::cout << "Игрок " << player->getName() << " не владеет этой улицей.\n";
+    }
+    if (status == 1)
+    {
+        std::cout << "На улице " << street->getName() << " нет построек для сноса.\n";
+    }
+    if (status == 2)
+    {
+        std::cout << "Игрок " << player->getName() << " снес одну структуру на " << street->getName() << ".\n";
+    }
+}
+
+void renderPlayerMakeBid(int currentHighestBid, Player *player, int status, int bid)
+{
+    switch (status)
+    {
+    case 0:
+        std::cout << player->getName() << ", текущая наивысшая ставка: " << currentHighestBid << ". Введите вашу ставку или -1 для выхода: ";
+        std::cout << "Наименьшая ставка 10М. \n";
+        break;
+    case 1:
+        std::cout << player->getName() << " выходит из аукциона.\n";
+        break;
+    case 2:
+        std::cout << player->getName() << " сделал ставку: " << bid << "\n";
+        break;
+    case 3:
+        std::cout << player->getName() << ", Ваш баланс должен быть не меньше ставки, которую вы хотите сделать. Снизьте ставку.\n";
+        break;
+    case 4:
+        std::cout << "У " << player->getName() << ", не хватает денег, он выходит из аукциона.\n";
+        break;
+    case 5:
+        std::cout << player->getName() << ", Ставка должна быть выше текущей наивысшей ставки.\n";
+        break;
+    default:
+        std::cout << "Неизвестный статус.\n";
+        break;
+    }
+}
+
+void renderPlayerMakeDicision(Player *player)
+{
+    std::cout << "Игрок " << player->getName() << ", вы попали на недвижимость.\n";
+    std::cout << "Выберите действие:\n";
+    std::cout << "0 - Купить недвижимость\n";
+    std::cout << "1 - Начать аукцион\n";
+    std::cout << "Введите ваш выбор: ";
+}
+
+void renderPlayerErrirMakeDicision()
+{
+    std::cout << "Неверный выбор. Пожалуйста, введите 0, 1 или 2: ";
+}
+
+void rednerPlayerStartAuction(int status, Player *player, Property *property, int highestBid)
+{
+    switch (status)
+    {
+    case 0:
+        std::cout << "Начинается аукцион за " << property->getName() << " с начальной ценой " << property->getPrice() << "\n";
+        break;
+    case 1:
+        std::cout << player->getName() << ", ваш текущий баланс: " << player->getBalance() << ".\n";
+        break;
+    case 2:
+        std::cout << player->getName() << " выигрывает аукцион и покупает " << property->getName() << " за " << highestBid << "\n";
+        break;
+    case 3:
+        std::cout << "Никто не сделал ставку. Аукцион завершен без победителя.\n";
+        break;
+    default:
+        std::cout << "Неизвестный статус.\n";
+        break;
+    }
+}
+
+void renderpPlayerMortgageProperty(Property *property, Player* player) {
+    std::cout << "Игрок " << player->getName() << " заложил имущество \"" << property->getName()
+              << "\" за " << property->calculateMortgage() << " монет.\n";
+}
 
 
-// std::vector<std::string> listOFstreets = {
-//     "FP", // Free Parking
-//     "KA", // Kentucky Avenue
-//     "CH", // Chance
-//     "IA", // Indiana Avenue
-//     "IL", // Illinois Avenue
-//     "BR", // B&O Railroad
-//     "AA", // Atlantic Avenue
-//     "VA", // Ventnor Avenue
-//     "WW", // Water Works
-//     "MG", // Marvin Gardens
-//     "GJ", // Go to Jail
-//     "NY", // New York Avenue
-//     "PA", // Pacific Avenue
-//     "TA", // Tennessee Avenue
-//     "NC", // North Carolina Avenue
-//     "CC", // Community Chest
-//     "SJ", // St. James Place
-//     "PA", // Pennsylvania Avenue
-//     "PR", // Pennsylvania Railroad
-//     "SL", // Short Line
-//     "VA", // Virginia Avenue
-//     "SA", // States Avenue
-//     "PP", // Park Place
-//     "EC", // Electric Company
-//     "LT", // LUXURY TAX
-//     "SC", // St. Charles Place
-//     "J",  // Jail
-//     "BW", // Boardwalk
-//     "JV", // Just Visiting
-//     "CA", // Connecticut Avenue
-//     "VA", // Vermont Avenue
-//     "OA", // Oriental Avenue
-//     "RR", // Reading Railroad
-//     "CT", // City Tax
-//     "BA", // Baltic Avenue
-//     "MA"  // Mediterranean Avenue
-// };
+void renderpPlayerUnmortgageProperty(Property *property, Player* player) {
+    std::cout << "Игрок " << player->getName() << " выкупил имущество \"" << property->getName()
+              << "\" за " << property->calculateUnMortgage() << " монет.\n";
+}
 
-// void drawBoard()
-// {
-//     int index = 0;
-//     for (int i = 0; i < 3; i++)
-//     {
-//         for (int j = 0; j < 10; j++)
-//         {
-//             if (i == 0 || i == 2)
-//             {
-//                 printf("_______ ");
-//                 continue;
-//             }
-//             else
-//             {
-//                 std::cout << "  " << listOFstreets[index] << "   |";
-//                 index++;
-//             }
-//         }
-//         printf("\n");
-//     }
-//     for (int i = 0; i < 10; i++)
-//     {
-//         for (int j = 0; j < 3; j++)
-//         {
-//             if (j == 0 || j == 2)
-//             {
-//                 printf("______");
-//                 printf("\t\t\t\t\t\t\t\t\t  ");
-//                 printf("_______\n");
-//                 continue;
-//             }
-//             else
-//             {
-//                 std::cout << " " << listOFstreets[index] << "   |";
-//                 printf("\t\t\t\t\t\t\t\t\t  ");
-//                 std::cout << " " << listOFstreets[index + 1] << "   |" << std::endl;
-//                 index++;
-//                 index++;
-//                 continue;
-//             }
-//         }
-//     }
 
-//     for (int i = 0; i < 3; i++)
-//     {
-//         for (int j = 0; j < 10; j++)
-//         {
-//             if (i == 0 || i == 2)
-//             {
-//                 printf("_______ ");
-//                 continue;
-//             }
-//             else
-//             {
-//                 std::cout << "  " << listOFstreets[index] << "   |";
-//                 index++;
-//             }
-//         }
-//         printf("\n");
-//     }
-// }
-
-// void displayPlayerInfo(const Player &player)
-// {
-//     std::cout << "Игрок: " << player.getName() << "\n";
-//     std::cout << "Баланс: " << player.getBalance() << "\n";
-//     std::cout << "Позиция на поле: " << player.getPosition() << "\n";
-//     std::cout << "В тюрьме: " << (player.isInJail() ? "Да" : "Нет") << "\n";
-//     std::cout << "Банкрот: " << (player.isBankrupt() ? "Да" : "Нет") << "\n";
-//     std::cout << "Недвижимость:\n";
-//     for (const Property *property : player.getProperties())
-//     {
-//         std::cout << " - " << property->getName() << "\n";
-//     }
-//     std::cout << "-------------------------------------\n";
-// }
-
-// void RenderCreateCell(std::string s, int flag)
-// {
-
-//     for (int index = 0; index < 10; index++)
-//     {
-//         if (flag == 1)
-//         {
-//             printf("                                             \n");
-//         }
-//         if (flag == 2)
-//         {
-//             printf("\n");
-//         }
-//         if (index == 0 || index == 9)
-//         {
-//             printf("____");
-//         }
-//         else if (index == 5)
-//         {
-//             printf("| %s |", s);
-//         }
-//         else
-//         {
-//             printf("          ");
-//         }
-//     }
-// }
-
-// void displayMenu()
-// {   
-//     std::cout << "Возможные действия в игре 'Монополия':n";
-//     std::cout << "1. Продать собственность";
-//     std::cout << "2. Построить дом/отель";
-//     std::cout << "3. Выйти из тюрьмы";
-//     std::cout << "4. Остаться в тюрьме";
-//     std::cout << "10. Объявить банкротствоn";
-//     std::cout << "11. Сделать предложение другим игрокамn";
-//     std::cout << "12. Применить карточку 'Выход из тюрьмы бесплатно'n";
-//     std::cout << "13. Сделать ход назад (если предусмотрено правилами)n";
-//     std::cout << "14. Завершить ходn";
-// }
-
-// void WelcomeThePlayers(std::vector<Player *> players)
-// {
-//     std::cout << "____________________________________________________________________________________________________________________" << std::endl;
-//     std::cout << "Welcome To Monopoly!!!!!" << std::endl;
-//     std::cout << "____________________________________________________________________________________________________________________" << std::endl;
-//     std::cout << "WARNING!!!!Before we start,please make the console window bigger in order to see the whole stuff...Width:140 and Height:60 should be OK!" << std::endl;
-//     std::cout << "If you have done the previous step,we are about to start" << std::endl;
-//     std::cout << "____________________________________________________________________________________________________________________" << std::endl;
-    
-// }
+void rednerplayerMoveToNearestStation(int status, int position){
+    if (status == 0) {
+         std::cout << "Игрок перемещен на ближайшую станцию на позиции " << position << ".\n";
+    }
+    else {
+         std::cout << "Станции не найдены на игровом поле.\n";
+    }
+}
