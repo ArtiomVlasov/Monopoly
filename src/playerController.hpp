54f6b021366eced4e7a18e431229cdbde1569d7e
localhost808O <91614028+localhost808O@users.noreply.github.com>
2024-11-21 14:34:19 +0700

    Update playerController.hpp

--- src/playerController.hpp
+++ src/playerController.hpp
@@ -7,18 +7,18 @@ class playerController{
 public:
 
     void playerMakeMove(int steps, Player* player);
-    void playerPay(int amount, Player* player);
-    void playerReceive(int amount, Player* player);
-    void playerAddProperty(Property* property, Player* player);
-    Player::AffordStatus playerCanAfford(int amount, Player* player);
-    void playerDeclareBankruptcy(Player *creditor);
+    static void playerPay(int amount, Player* player);
+    static void playerReceive(int amount, Player* player);
+    static void playerAddProperty(Property* property, Player* player);
+    static Player::AffordStatus playerCanAfford(int amount, Player* player);
+    static void playerDeclareBankruptcy(Player *creditor);
     void buy(Property *property); //добавить
     bool playerCanBuildOn(Property* property, Player* player) const;
     void playerBuildStructure(Street* street, Player* player);
     void playerDestroyStructure(Street* steet, Player* player);
-    int playerMakeBid(int currentHighestBid, Player *player);
-    int playerMakeDicision(Player* player);
-    void playerStartAuction(Property *property, const std::vector<Player *> &players, Player* player);
+
+    static int playerMakeDicision(Player* player);
+    static void playerStartAuction(Property *property, const std::vector<Player *> &players, Player* player);
     void playerMortgageProperty(Property* property, Player* player);
     void playerUnmortgagedProperty(Property* property, Player* player);
     void playerMoveToNearestStation(Game* game, int posIndex, Player* player);
