cf48355554b629a142e7376acf8fb0d1a14d88bf
localhost808O <91614028+localhost808O@users.noreply.github.com>
2024-11-21 12:49:51 +0700

    убрал методы которые есть в контроллере

--- src/player.hpp
+++ src/player.hpp
@@ -16,14 +16,11 @@ private:
     int balance;
     int position;
     std::vector<Property *> listOfProperty;
-    // bool inJail;// перемистить в Prison
-    bool bankrupt;
     int totalPriceOfProperty;
-    int totalDiceRoll;
-    uint8_t numMovesInPrison;
+    // int totalDiceRoll;
+
 
 public:
-
 
     enum AffordStatus
     {
@@ -34,15 +31,15 @@ public:
 
     Player(std::string name);
     Player();
-    void setInJael(bool flag); //????????????????????????
-    void makeMove(int steps);
+    //void setInJael(bool flag); //????????????????????????
+    //void makeMove(int steps);
     void setPosition(int pos);
-    void addProperty(Property *property);
-    void pay(int amount);
-    void receive(int amount);
-    AffordStatus canAfford(int amount);
-    void mortgageProperty(Property *property);
-    void unmortgagedProperty(Property *property);
+    //void addProperty(Property *property);
+    //void pay(int amount);
+    //void receive(int amount);
+    //AffordStatus canAfford(int amount);
+    // void mortgageProperty(Property *property);
+    // void unmortgagedProperty(Property *property);
     int getBalance() const;
     int getTotalPriceOfProperty();
     void buy(Property *property);
@@ -51,28 +48,27 @@ public:
     int getPosition() const;
     const std::vector<Property *> &getProperties() const;
     int getQuantityOfProperty();
-    bool isBankrupt() const;
-    bool isInJail() const;
-    void sendToJail();
-    void releaseFromJail();
+    // bool isBankrupt() const;
+    // bool isInJail() const;
+    // void sendToJail();
+    // void releaseFromJail();
     bool canBuildOn(Property *property) const;
-    void buildStructure(Street *property);
-    void destroyStructure(Street *street);
-    int getNumMovesInPrison();
-    void incrementNumMovesInPrison();
-    void declareBankruptcy(Player *creditor = nullptr);
-    int makeDecision();
-    void startAuction(Property *property, const std::vector<Player *> &players);
-    void moveToNearestStation(Game *game, int posIndex);
+    // void buildStructure(Street *property);
+    // void destroyStructure(Street *street);
+    // int getNumMovesInPrison();
+    // void incrementNumMovesInPrison();
+    //void declareBankruptcy(Player *creditor = nullptr);
+    // int makeDecision();
+    // void startAuction(Property *property, const std::vector<Player *> &players);
+    // void moveToNearestStation(Game *game, int posIndex);
     int getNumberOfHouses() const;
     int getNumberOfHotels() const;
     int getOwnedPropertyCount(CellType type);
-    int getLastDiceRoll();
-    void setDiceRoll(Game *game);
+
     void payToExit(int jailFee);
     void setBalance(int amount);
     void setTotalPriceOfProperty(int price);
-    void setBankrupt();
+    //void setBankrupt();
     std::vector<Property *> getListOfProperty();
     void pushListOfProperty(Property* property);
     ~Player();
