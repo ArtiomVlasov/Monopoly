e7257d2ffe959d05c54f441de2591df09ae72158
localhost808O <91614028+localhost808O@users.noreply.github.com>
2024-11-23 12:44:54 +0700

    Update game.hpp

--- src/game.hpp
+++ src/game.hpp
@@ -1,28 +1,33 @@
 #include <vector>
 #include <memory>
 #include "board.hpp"
+class prisonController;
+class playerController;
+class StreetController;
 #pragma once
 class Player;
 
 class Game {
 public:
+    
     Game(int numPlayers, int numCells);
     void start();
     std::vector<Player *> getListOfPlayers();
-    void takeTurn();
+    void takeTurn(prisonController *pc, playerController* playercontroller, prisonController* prisoncontroller);
     int getBoardSize();
     Board getBoard();
     bool isGameOver() const; 
     void sellProperty(Player* player);
-    int getRollDice();
+    static int getRollDice();
     int rollDice();
     static bool isBankruptPlayer(Player* player);
     static void addBankruptPlayers(Player* player);
+    bool isAllPlayersIsBankrupt;
 private:
     static std::vector<Player *> bankruptPlayers;
-    void caseMapToAction(int a);
-    int firstValue;
-    int secondValue;
+    void caseMapToAction(int a, Player* player, playerController* playercontooller, prisonController* prisoncontroller);
+    static int firstValue;
+    static int secondValue;
     int playerTurn;
     std::vector<Player *> players;
     Board* board;
