56f98422d23666462998f1f1d10252ea16d1377a
localhost808O <91614028+localhost808O@users.noreply.github.com>
2024-11-23 13:15:13 +0700

    Update board.hpp

--- src/board.hpp
+++ src/board.hpp
@@ -14,7 +14,7 @@ public:
     Board();
     Board(int numCells, Game *game);
     std::shared_ptr<Cell> getCell(int position) const;
-    Street* getStreetByName(std::string nameStreet);
+    Street* getStreetByName(std::string m);
     int getNextPosition(int currentPosition, int roll) const;
     std::vector<std::shared_ptr<Cell>> getAllCells();
 };
