d28c64e06dea640fc91ad0fdc70bb6262ccbef5b
localhost808O <91614028+localhost808O@users.noreply.github.com>
2024-11-23 12:46:39 +0700

    Update prisonController.hpp

--- src/prisonController.hpp
+++ src/prisonController.hpp
@@ -7,8 +7,9 @@ private:
 public:
     prisonController(Prison *prison);
     void playerReleaseFromJail(Player *player);
-    bool isInJail(Player* player);
+    static bool isInJail(Player* player);
     void payToExit(Player* player);
-    static void addPlayerInPrison(Player *player, Prison *prison);
+    static void addPlayerInPrison(Player *player);
+    static Prison* getPrison();
 };
 
